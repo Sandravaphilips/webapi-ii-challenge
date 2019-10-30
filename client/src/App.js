d62f60f330e9163599ab339ebee88958d980a652
Sandrava Philips <lacasandra1@gmail.com>
2019-10-30 12:49:13 +0100

    installed react app dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
 import './App.css';
 
+const AppStyle = styled.div`
+  div{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 15px 20%;
+
+    div{
+      border: 1px solid black;
+      background-color: lightgreen;
+      border-radius:8px;
+      width: 200px;
+    }
+  }
+`
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:500/api/posts')
+    .then(res => {
+      setPosts(res.data)
+    })
+    .catch(err => console.log(err))
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyle className="App">
+      <h1>Welcome To My Posts' Page</h1>
+
+      <div>
+        {posts.map(post => {
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.contents}</p>
+          </div>
+        })}
+      </div>
+    </AppStyle>
   );
 }
 
